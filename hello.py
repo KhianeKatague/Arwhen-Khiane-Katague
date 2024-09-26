b4427a08452952e0b9eb986fb7c889256f1ad114
Khiane Katague <kataguearwhen@gmail.com>
2024-09-26 14:27:43 +0800

    first upload or file

---
+++ hello.py
@@ -0,0 +1,23 @@
+# this command is for displaying output
+
+print("Hello, World!")
+
+#printing c/o of variables
+
+first_name = "Khiane"
+last_name = "Katague"
+
+print(first_name)
+print(last_name)
+
+print(first_name , last_name)
+print(first_name , last_name)
+
+x = "and awesome"
+
+def my_class():
+    y = "great"
+    print("Python is " + y)
+   
+my_class()
+print(x) 
---
+++ laboratory.py
@@ -0,0 +1,13 @@
+
+x = "Khiane"
+y = "Katague"
+z = "19"
+a = x +" "+ y
+
+print("Hello, my name is" , x , y, "I am" , z , "years old")
+
+if (a.isupper == True):
+    print(a)
+else:
+    b = a.upper()
+    print(b)
